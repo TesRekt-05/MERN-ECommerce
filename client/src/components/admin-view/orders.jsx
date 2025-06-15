c8c3d8c663a2123772aeed694d4a19a8ddd001b0
TesRekt-05 <iffcogc34@gmail.com>
2025-06-16 01:05:23 +0530

    feat: Add order details view for admin and shopping interfaces with dialog integration

--- client/src/components/admin-view/orders.jsx
+++ client/src/components/admin-view/orders.jsx
@@ -1,8 +1,55 @@
-function AdminOrders() {
+import { Dialog, DialogContent, DialogTitle } from "@radix-ui/react-dialog";
+import { Button } from "../ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { TableHead, TableHeader, TableRow, Table, TableBody, TableCell } from "../ui/table";
+import { useState } from "react";
+import AdminOrderDetailsView from "./order-details";
+
+
+function AdminOrdersView() {
+
+    const [openDetailsDialog, setOpenDetailsDialog] = useState(false);
+
     return (
-        <div className="">
-            AdminOrders
-        </div>
+        <Card>
+            <CardHeader>
+                <CardTitle> All Orders</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead>Order ID</TableHead>
+                            <TableHead>Order Date</TableHead>
+                            <TableHead>Order Status</TableHead>
+                            <TableHead>Order Price</TableHead>
+                            <TableHead >
+                                <span className="sr-only">Details</span>
+                            </TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell>123456</TableCell>
+                            <TableCell>15/06/2025</TableCell>
+                            <TableCell>In Process</TableCell>
+                            <TableCell>$1000</TableCell>
+                            <TableCell>
+                                <Dialog open={openDetailsDialog} onOpenChange={setOpenDetailsDialog}>
+                                <Button onClick={() => setOpenDetailsDialog(true)}>
+                                    View Details
+                                </Button>
+                                <DialogContent>
+                                  <DialogTitle>Order Details</DialogTitle>
+                                  <AdminOrderDetailsView/>
+                                </DialogContent>
+                                </Dialog>
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </CardContent>
+        </Card>
     )
 }
-export default  AdminOrders;
+export default AdminOrdersView;
--- client/src/components/shopping-view/orders.jsx
+++ client/src/components/shopping-view/orders.jsx
@@ -1,14 +1,33 @@
-import { Table } from "lucide-react";
+import { Dialog } from "../ui/dialog";
+import { Button } from "../ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
-import { TableHead, TableHeader, TableRow } from "../ui/table";
-
-
-
-
+import { TableHead, TableHeader, TableRow, Table, TableBody, TableCell } from "../ui/table";
+import ShoppingOrderDetailsView from "./order-details";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 
 
 function ShoppingOrders() {
+    const [openDetailsDialog, setOpenDetailsDialog] = useState(false);
+    const dispatch = useDispatch();
+    // const { user } = useSelector((state) => state.auth);
+    // const { orderList, orderDetails } = useSelector((state) => state.shopOrder);
+
+    // function handleFetchOrderDetails(getId) {
+    //     dispatch(getOrderDetails(getId));
+    // }
+
+    // useEffect(() => {
+    //     dispatch(getAllOrdersByUserId(user?.id));
+    // }, [dispatch]);
+
+    // useEffect(() => {
+    //     if (orderDetails !== null) setOpenDetailsDialog(true);
+    // }, [orderDetails]);
+
+    // console.log(orderDetails, "orderDetails");
+
     return (
         <Card>
             <CardHeader>
@@ -22,11 +41,32 @@ function ShoppingOrders() {
                             <TableHead>Order Date</TableHead>
                             <TableHead>Order Status</TableHead>
                             <TableHead>Order Price</TableHead>
-                            <TableHead>
+                            <TableHead >
                                 <span className="sr-only">Details</span>
                             </TableHead>
                         </TableRow>
                     </TableHeader>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell>123456</TableCell>
+                            <TableCell>15/06/2025</TableCell>
+                            <TableCell>In Process</TableCell>
+                            <TableCell>$1000</TableCell>
+                            <TableCell>
+                                <Dialog open={openDetailsDialog}
+                                    onOpenChange={() => {
+                                        setOpenDetailsDialog(false);
+                                        // dispatch(resetOrderDetails());
+                                    }}>
+                                    <Button onClick={() =>
+                                        setOpenDetailsDialog(true)}>
+                                        View Details
+                                    </Button>
+                                    <ShoppingOrderDetailsView />
+                                </Dialog>
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
                 </Table>
             </CardContent>
         </Card>
